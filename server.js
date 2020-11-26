c293d300fc7e9d9cf2f35e00fcc1235e02d75e17
matix7290 <mateusz7290.mo@gmail.com>
2020-11-26 14:13:50 +0100

    repair connection whit Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require("express")
 var app = express()
-const PORT = 3000
+var PORT = process.env.PORT || 3000
 var path = require("path")
 var bodyParser = require("body-parser")
 var cookieParser = require("cookie-parser")
